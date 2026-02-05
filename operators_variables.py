ba4bb66d9057ae15c71e42a2e2a2104dfa51f917
AmmarShaikh81 <shaikhammar07860@gmail.com>
2026-02-05 15:13:55 +0530

    Added operators and variables in python files

---
+++ operators_variables.py
@@ -0,0 +1,18 @@
+#declaring and initialising variables
+a=5
+d=6
+b3='ammar'
+a1,b1,c1=3,'roy','kapur'
+x=True
+print(a+d)
+print(a1,b1,c1,)
+print(b3)
+#swapping the variables
+a=3
+b=1
+a,b=b,a
+print(a,b)
+print(type(a))
+print(type(b3))
+print(type(x))
+
