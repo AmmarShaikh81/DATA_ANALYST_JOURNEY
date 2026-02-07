c0fd0e3e0525ca6175f60d18eeecc2e99cf98f6d
AmmarShaikh81 <shaikhammar07860@gmail.com>
2026-02-07 15:10:07 +0530

    Sql basic queries

--- select.sql
+++ select.sql
@@ -10,3 +10,9 @@ INSERT INTO students VALUES (1, 'Ryan', 22,'A','Maths');
 INSERT INTO students VALUES (2, 'Joanna',16,'B','Arabic');
 -- fetch some values
 SELECT * FROM students;
+--where
+select * from students where age=22;
+--by using limit
+select * from students where age=22 limit 2;
+--by using order by
+select * from students where grade='A' order by age desc;
