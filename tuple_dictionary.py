a31e82c65930fe4b4f8a704b1f9359d2e7627ebf
AmmarShaikh81 <shaikhammar07860@gmail.com>
2026-02-07 14:43:53 +0530

    tuples and dictionary

---
+++ tuple_dictionary.py
@@ -0,0 +1,10 @@
+#below both of them are tuple
+a=1,2,3
+a1=(34,4,1)
+print(type(a))
+print(type(a1))
+#dictionary or hashmap
+dict={'name':'aryan','num':54,'address':'pune'}
+print(dict['num'])
+dict['roll_no']=44
+print(dict)
